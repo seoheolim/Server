90bccd15b10f82d266deb0644ae136eee9511a75
JEONGHO LIM <jeongholim@JEONGHOui-MacBookPro.local>
2022-11-16 17:22:45 +0900

    feat: apply email function

--- app/api/tasks.py
+++ app/api/tasks.py
@@ -1,23 +1,30 @@
 import logging
+import json
+
 from fastapi import UploadFile
 
 from celery import shared_task
+import asyncio
 
 from .s3 import upload_service
+from .email import send_email
 
 
-async def process_files(video_file: UploadFile, image_file: UploadFile, task_id):
+async def process_files(video_file: UploadFile, image_file: UploadFile, email: str, task_id: str):
     response = upload_service.upload_video_file(image_file, video_file, "before/")
-    convert_video.apply_async([response], task_id=task_id)
+    convert_video.apply_async([response, email], task_id=task_id)
 
 
 @shared_task
-def convert_video(loc):
-    import time
-    time.sleep(5)
-    # todo
-    print("done!")
+def convert_video(loc, email):
+    '''
+        todo: 모델 연동하는 부분
+    '''
+
     logging.info(f"convert video done! {loc}")
+
+    logging.info(f"email sent! {loc}")
+
+    asyncio.run(send_email(email, json.dumps(loc)))
+
     return loc
-
-
